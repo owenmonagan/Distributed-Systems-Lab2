556f8a2e71340e2dabf887d8be24737d676bd688
owenmonagan <owenmonagan@gmail.com>
2015-10-18 15:39:36 +0100

    basic server with client working in python

--- skeleton.py
+++ skeleton.py
@@ -1,46 +1,20 @@
-import socket
-import threading
 import socketserver
 
-class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
-
+class skelServer(socketserver.BaseRequestHandler):
+    """Request handler for my server"""
     def handle(self):
-        data = str(self.request.recv(1024), 'ascii')
-        cur_thread = threading.current_thread()
-        response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
-        self.request.sendall(response)
-
-class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-    pass
-
-def client(ip, port, message):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((ip, port))
-    try:
-        sock.sendall(bytes(message, 'ascii'))
-        response = str(sock.recv(1024), 'ascii')
-        print("Received: {}".format(response))
-    finally:
-        sock.close()
+        self.clientInfo= self.request.recv(1024).strip()
+        print ("{} wrote:".format(self.client_address[0]))
+        print (self.clientInfo.decode("ascii"))
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.clientInfo.upper())
 
 if __name__ == "__main__":
-    # Port 0 means to select an arbitrary unused port
-    HOST, PORT = "localhost", 0
+    HOST, PORT = "localhost", 9999
 
-    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
-    ip, port = server.server_address
+    # Create the server, binding to localhost on port 9999
+    server = socketserver.TCPServer((HOST, PORT), skelServer)
 
-    # Start a thread with the server -- that thread will then start one
-    # more thread for each request
-    server_thread = threading.Thread(target=server.serve_forever)
-    # Exit the server thread when the main thread terminates
-    server_thread.daemon = True
-    server_thread.start()
-    print("Server loop running in thread:", server_thread.name)
-
-    client(ip, port, "Hello World 1")
-    client(ip, port, "Hello World 2")
-    client(ip, port, "Hello World 3")
-
-    server.shutdown()
-    server.server_close()
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
